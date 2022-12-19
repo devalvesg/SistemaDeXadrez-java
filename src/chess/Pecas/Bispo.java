db27c2b7ff68e8f4680861fec86abd0399d4491b
devalvesg <gabrieldamasceno881@outlook.com>
2022-12-19 11:00:12 -0300

    Bispo

---
+++ src/chess/Pecas/Bispo.java
@@ -0,0 +1,65 @@
+package chess.Pecas;
+
+import TabuleiroJogo.Posicao;
+import TabuleiroJogo.Tabuleiro;
+import chess.Cor;
+import chess.chessPeca;
+
+public class Bispo extends chessPeca{
+
+	public Bispo(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+	
+	@Override
+	public boolean[][] possiveisMovimentos() {
+		boolean [][] mat = new boolean[getTabuleiro().getLinhas()] [getTabuleiro().getColunas()]; 
+		Posicao p = new Posicao(0, 0);
+		
+		//NW
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && existePecaDoOponente(p)) {
+			mat [p.getLinha()] [p.getColuna()] = true;			
+		}
+		
+		//NE
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && existePecaDoOponente(p)) {
+			mat [p.getLinha()] [p.getColuna()] = true;			
+		}
+		
+		//SE
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && existePecaDoOponente(p)) {
+			mat [p.getLinha()] [p.getColuna()] = true;			
+		}
+		
+		//SW
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && existePecaDoOponente(p)) {
+			mat [p.getLinha()] [p.getColuna()] = true;			
+		}
+		return mat;
+	}
+}
